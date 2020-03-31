7398cff3cc1dbdeb60eaea294fc5a35817ee7ced
leonheuler <ironfelix1926@gmail.com>
2020-03-31 23:54:59 +0300

    19 upd

--- 19/main.cpp
+++ 19/main.cpp
@@ -4,22 +4,45 @@
 
 using namespace std;
 
+int months_to_sundays(int n)
+{
+    int month[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    int sundays = 0;
+    int extra_days = 0;
+
+    int year = 1900;
+
+    for (int i = 0; i < n; i++)
+    {
+        if ((year % 100) == 0)
+        {
+            if ((year % 400) == 0)
+                month[1] = 29;
+        }
+        else if ((year % 4) == 0)
+            month[1] = 29;
+        else
+            month[1] = 28;
+        if (i % 12 == 0 && i != 0)
+            year++;
+
+        sundays += month[i % 13] / 7 + (month[i % 13] % 7 + extra_days) / 7;
+        extra_days += month[i % 13] % 7;
+
+        if (extra_days > 7)
+        {
+            sundays += extra_days / 7;
+            extra_days = extra_days % 7;
+        }
+        cout << "Year: " << year << ", month: " << i % 12 + 1 << ", sundays: " << sundays << ", extra_days: " << extra_days << endl;
+    }
+}
+
 int main()
 {
 
-    typedef vector<vector<int>> mat_t;
+    months_to_sundays(5);
 
-    mat_t mat = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-
-    for (mat_t::const_iterator ii = mat.begin();
-         ii != mat.end(); ++ii)
-    {
-        for (vector<int>::const_iterator jj = ii->begin();
-             jj != ii->end(); ++jj)
-        {
-            cout << *jj;
-        }
-        cout << endl;
-    }
     return 0;
 }
