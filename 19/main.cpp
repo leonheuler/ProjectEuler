8b85d66b8ada078a2eea7fa9a09efaf6884eaab0
leonheuler <ironfelix1926@gmail.com>
2020-04-01 09:50:37 +0300

    upd

--- 19/main.cpp
+++ 19/main.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <vector>
-#include <string>
 
 using namespace std;
 
-int months_to_sundays(int n)
+
+int count_sundays(int n)
 {
     int month[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
     int sundays = 0;
     int extra_days = 0;
-
+    int sundays_on_first_of_month = 0;
     int year = 1900;
 
     for (int i = 0; i < n; i++)
@@ -24,25 +24,27 @@ int months_to_sundays(int n)
             month[1] = 29;
         else
             month[1] = 28;
+
         if (i % 12 == 0 && i != 0)
             year++;
 
-        sundays += month[i % 13] / 7 + (month[i % 13] % 7 + extra_days) / 7;
-        extra_days += month[i % 13] % 7;
+        sundays += month[i % 12] / 7 + (month[i % 12] % 7 + extra_days) / 7;
+
+        extra_days += month[i % 12] % 7;
 
         if (extra_days > 7)
-        {
-            sundays += extra_days / 7;
             extra_days = extra_days % 7;
-        }
+
+        if (extra_days == 6)
+            sundays_on_first_of_month++;
+
         cout << "Year: " << year << ", month: " << i % 12 + 1 << ", sundays: " << sundays << ", extra_days: " << extra_days << endl;
     }
+    return sundays_on_first_of_month;
 }
 
 int main()
 {
-
-    months_to_sundays(5);
-
+    cout << "Answer: " << count_sundays(1212) - count_sundays(12) << endl;
     return 0;
 }
