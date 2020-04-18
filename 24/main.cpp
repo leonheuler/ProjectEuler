ce60c0f5d1812b22b65f8e3805a2799e0851bb28
leonheuler <ironfelix1926@gmail.com>
2020-04-19 02:47:58 +0300

    24

--- 24/main.cpp
+++ 24/main.cpp
@@ -11,24 +11,52 @@
 
 using namespace std;
 
-#define LIMIT 5
+#define print_vec(v)                                            \
+    copy(v.begin(), v.end(), ostream_iterator<int>(cout, " ")); \
+    cout << endl
+
+#define len 5
+
+#define print_mat(m)                          \
+    for (size_t r = 0; r < fact(len); r++)     \
+    {                                         \
+        for (size_t c = 0; c < fact(len); c++) \
+            cout << m[r][c];                  \
+        cout << endl;                         \
+    }
 
 unsigned long long fact(int n)
 {
     unsigned long long ret = 1;
-    for (int i = 2; i <= n; ++i)
+    for (int i = 2; i <= n; i++)
         ret *= i;
     return ret;
 }
 
 int main()
 {
-    static const long size = fact(LIMIT);
+    // const int n = 5;
+    const long fact_len = fact(len);
     int **m;
-    m = new int *[size];
-    for (size_t i = 0; i < size; i++)
-        m[i] = new int[LIMIT];
+    m = new int *[fact_len]; // n! строк
+    for (size_t i = 0; i < len; i++)
+        m[i] = new int[len];
 
+    vector<int> v;
+    for (int i = 0; i < len; i++)
+        v.push_back(i);
+    print_vec(v);
+
+    for (size_t col = 0; col < len - 2; col++)
+    {
+        for (size_t row = 0; row < fact_len; row++)
+        {
+            m[row][col] = 1;
+        }
+    }
+    print_mat(m);
+
+    /*
     for (size_t row = 0; row < size; row++)
         for (int col = 0; col < LIMIT; col++)
             m[row][col] = col;
@@ -51,13 +79,15 @@ int main()
             }
         }
     }
+    */
     ofstream fileo("mat");
 
-    for (size_t row = 0; row < size; row++)
-    {
-        for (int col = 0; col < LIMIT; col++)
-            fileo << m[row][col] << " ";
-        fileo << endl;
-    }
+    // for (size_t row = 0; row < size; row++)
+    // {
+    //     for (int col = 0; col < LIMIT; col++)
+    //         fileo << m[row][col] << " ";
+    //     fileo << endl;
+    // }
+
     return 0;
 }
