579b607e883ee34a3c34106e695255ab80ff31eb
leonheuler <leonheuler@gmail.com>
2020-04-21 17:08:31 +0300

    24 solved

--- 24/main.cpp
+++ 24/main.cpp
@@ -1,93 +1,88 @@
-/** 24
- * abcdefghij -> ... -> jihgfedcba */
-
-#include <iostream>
-#include <string>
 #include <vector>
-#include <iterator>
+#include <map>
 #include <algorithm>
-#include <math.h>
+#include <iostream>
+#include <iterator>
 #include <fstream>
 
 using namespace std;
 
-#define print_vec(v)                                            \
-    copy(v.begin(), v.end(), ostream_iterator<int>(cout, " ")); \
-    cout << endl
+typedef unsigned long num_t;
+typedef vector<num_t> vec_t;
+typedef vec_t seed_t;
+typedef pair<vec_t, seed_t> block_t;
 
-#define len 5
+vector<block_t> grow_seed(const seed_t &);
+vector<block_t> grow_row(const vector<block_t> &);
+vector<vector<block_t>> transpose(const vector<vector<block_t>> &);
+void print(const vector<block_t> &);
+void print_block(const block_t &);
+void print_vec(const vec_t &);
+size_t fact(num_t);
 
-#define print_mat(m)                          \
-    for (size_t r = 0; r < fact(len); r++)     \
-    {                                         \
-        for (size_t c = 0; c < fact(len); c++) \
-            cout << m[r][c];                  \
-        cout << endl;                         \
-    }
-
-unsigned long long fact(int n)
-{
-    unsigned long long ret = 1;
-    for (int i = 2; i <= n; i++)
-        ret *= i;
-    return ret;
-}
+ofstream fileo("result.txt");
 
 int main()
 {
-    // const int n = 5;
-    const long fact_len = fact(len);
-    int **m;
-    m = new int *[fact_len]; // n! строк
-    for (size_t i = 0; i < len; i++)
-        m[i] = new int[len];
-
-    vector<int> v;
-    for (int i = 0; i < len; i++)
-        v.push_back(i);
-    print_vec(v);
-
-    for (size_t col = 0; col < len - 2; col++)
+    seed_t rootSeed = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    vector<vector<block_t>> mat = {grow_seed(rootSeed)};
+    print(mat[0]);
+    for (seed_t::size_type i = 0; i < rootSeed.size() - 1; ++i)
     {
-        for (size_t row = 0; row < fact_len; row++)
-        {
-            m[row][col] = 1;
-        }
+        vector<block_t> row = grow_row(mat[i]);
+        print(row);
+        mat.push_back(row);
     }
-    print_mat(m);
-
-    /*
-    for (size_t row = 0; row < size; row++)
-        for (int col = 0; col < LIMIT; col++)
-            m[row][col] = col;
-
-    int n = 0;
-    for (int col = 0; col < LIMIT - 3; col++)
-    {
-        const size_t lim_c = fact(LIMIT - col) / (LIMIT - col);
-        size_t lim = lim_c;
-        n = 0;
-        for (size_t row = 0; row < size; row++)
-        {
-            m[row][col] = n;
-            if (row == lim - 1)
-            {
-                n++;
-                if (n == LIMIT-col+1)
-                    n = 0;
-                lim += lim_c;
-            }
-        }
-    }
-    */
-    ofstream fileo("mat");
-
-    // for (size_t row = 0; row < size; row++)
-    // {
-    //     for (int col = 0; col < LIMIT; col++)
-    //         fileo << m[row][col] << " ";
-    //     fileo << endl;
-    // }
 
     return 0;
+}
+
+vector<block_t> grow_seed(const seed_t &rootSeed)
+{
+    vector<block_t> ret;
+    size_t fact_len = fact(rootSeed.size() - 1);
+    for (seed_t::size_type i = 0; i != rootSeed.size(); ++i)
+    {
+        seed_t seed = rootSeed;
+        seed.erase(find(seed.begin(), seed.end(), rootSeed[i]));
+        vec_t part(fact_len, rootSeed[i]);
+        block_t block = {part, seed};
+        ret.push_back(block);
+    }
+    return ret;
+}
+
+vector<block_t> grow_row(const vector<block_t> &prev_row)
+{
+    vector<block_t> ret;
+    vector<block_t>::size_type size = prev_row.size();
+    for (vector<block_t>::const_iterator it = prev_row.begin(); it != prev_row.end(); ++it)
+    {
+        vector<block_t> block = grow_seed(it->second);
+        copy(block.begin(), block.end(), back_inserter(ret)); 
+    }
+    return ret;
+}
+
+size_t fact(num_t n)
+{
+    return (n == 1 || n == 0) ? 1 : n * fact(n - 1);
+}
+
+
+void print(const vector<block_t> &v)
+{
+    for_each(v.begin(), v.end(), print_block);
+    fileo << endl;
+}
+
+void print_block(const block_t &b)
+{
+    print_vec(b.first);
+    // print_vec(b.second);
+}
+
+void print_vec(const vec_t &v)
+{
+    copy(v.begin(), v.end(), ostream_iterator<num_t>(fileo));
 }
